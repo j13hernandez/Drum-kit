939ed18a6733e17bf622d20139377b257defca6c
Palma <1hernandezj3@gmail.com>
2021-11-25 10:11:42 -0500

    Drum kit is functional, with key pressed and click events. The button fades a bit when activated to let user know

---
+++ index.js
@@ -0,0 +1,57 @@
+for (i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    let buttonPressed = this.innerHTML;
+    makeSound(buttonPressed);
+    buttonAnimation(buttonPressed);
+  });
+}
+
+document.addEventListener('keydown', function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key.toLowerCase()) {
+    case 'a':
+      let crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case 's':
+      let kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case 'd':
+      let snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'f':
+      let tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'j':
+      let tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 'k':
+      let tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'l':
+      let tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonPressed);
+      break;
+  }
+}
+function buttonAnimation(key) {
+  let activeKey = document.querySelector(`.${key.toLowerCase()}`);
+  activeKey.classList.add('pressed');
+
+  setTimeout(function () {
+    activeKey.classList.remove('pressed');
+  }, 100);
+}
